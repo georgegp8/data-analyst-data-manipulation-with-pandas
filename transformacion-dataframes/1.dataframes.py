fc002f0bd9dcdf1c025fbe611ab838a3fd35a91b
George Guerra <george.guerra@tecsup.edu.pe>
2026-03-26 22:27:51 -0500

    add csv files and first section to dataframes

---
+++ transformacion-dataframes/1.dataframes.py
@@ -0,0 +1,40 @@
+# ================================================
+# Ejercicios DataFrames - Part 1
+# ================================================
+
+# Import pandas using the alias pd
+import pandas as pd
+
+# Load the homelessness dataset
+homelessness = pd.read_csv('homelessness.csv')
+
+# ------------------------------------------------
+# Sección 1: Inspeccionar un DataFrame
+# ------------------------------------------------
+# Cuando se recibe un nuevo DataFrame para trabajar, lo primero que se debe hacer es explorar sus datos.
+# Hay varios métodos útiles que puedes usar para observar rápidamente el DataFrame:
+# - .head() devuelve las primeras filas (por defecto 5)
+# - .info() muestra información sobre las columnas, tales como el tipo de datos y los valores faltantes
+# - .shape devuelve el número de filas y columnas del DataFrame
+# - .describe() calcula algunas estadísticas resumidas para las columnas numéricas
+#
+# homelessness es un DataFrame que contiene estimaciones del número de personas sin hogar en cada estado de EE.UU.
+# en 2018. El índice individual es el número de fila por el cual se conoce cada fila.
+
+# Instrucciones:
+# - Imprime las primeras filas del DataFrame homelessness.
+# - Imprime información sobre los tipos de datos y valores faltantes de las columnas en homelessness.
+# - Imprime el número de filas y columnas en homelessness.
+# - Imprime algunas estadísticas resumidas que describen las columnas numéricas de homelessness.
+
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
