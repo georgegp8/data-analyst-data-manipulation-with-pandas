2e3a409de6edbb9e403e42ab1257cd9922852dc6
George Guerra <george.guerra@tecsup.edu.pe>
2026-03-28 18:01:23 -0500

    add second and third section to statistics systesis file

--- agregar-dataframes/1.sintesis-estadistica.py
+++ agregar-dataframes/1.sintesis-estadistica.py
@@ -1,5 +1,5 @@
 # ================================================
-# Ejercicios Síntesis Estadística - Part 1
+# Ejercicios Síntesis Estadística 
 # ================================================
 
 # Import pandas using the alias pd
@@ -34,3 +34,58 @@ print(sales["weekly_sales"].mean())
 
 # Print the median of weekly_sales
 print(sales["weekly_sales"].median())
+
+# ------------------------------------------------
+# Sección 2: Resumir fechas
+# ------------------------------------------------
+# Las estadísticas sumarias también pueden calcularse sobre columnas de fecha que tengan valores con el tipo
+# de datos datetime64. Algunas estadísticas sumarias, como la media, no tienen mucho sentido en las fechas,
+# pero otras son extremadamente útiles como, por ejemplo, el mínimo y el máximo, que te permiten ver qué
+# intervalo de tiempo abarcan tus datos.
+#
+# sales está disponible y pandas se carga como pd.
+
+# Instrucciones:
+# - Imprime el máximo de la columna date.
+# - Imprime el mínimo de la columna date.
+
+# Print the maximum of the date column
+print(sales["date"].max())
+
+# Print the minimum of the date column
+print(sales["date"].min())
+
+# ------------------------------------------------
+# Sección 3: Resúmenes eficaces
+# ------------------------------------------------
+# Aunque pandas y NumPy tienen muchas funciones, a veces puedes necesitar una función diferente para
+# resumir tus datos.
+#
+# El método .agg() te permite aplicar tus propias funciones personalizadas a un DataFrame, así como aplicar
+# funciones a más de una columna de un DataFrame a la vez, haciendo que tus agregaciones sean muy eficientes.
+# Por ejemplo:
+# - df['column'].agg(function)
+#
+# En la función personalizada para este ejercicio, "IQR" es la abreviatura de rango intercuartílico, que es
+# el percentil 75 menos el percentil 25. Es una alternativa a la desviación típica que resulta útil si tus
+# datos contienen valores atípicos.
+#
+# sales está disponible y pandas se carga como pd.
+
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+# Instrucciones:
+# - Utiliza la función personalizada iqr definida junto con .agg() para imprimir el IQR de la columna
+#   temperature_c de sales.
+# - Actualiza la selección de columnas para utilizar la función personalizada iqr con .agg() para imprimir
+#   el IQR de temperature_c, fuel_price_usd_per_l y unemployment, en ese orden.
+# - Actualiza las funciones de agregación llamadas por .agg(): incluir iqr y "median" en ese orden.
+
+# Print IQR of temperature_c
+print(sales["temperature_c"].agg(iqr))
+
+# Update to print IQR of temperature_c, fuel_price_usd_per_l, and unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg(iqr, "median"))
+
