ad4f9b88c1c0df46e2b1d851b3e33a652a6aa366
George Guerra <george.guerra@tecsup.edu.pe>
2026-04-07 23:20:57 -0500

    add second section to create dataframes file

--- 4.creacion-visualizacion-dataframes/3.crear-dataframes.py
+++ 4.creacion-visualizacion-dataframes/3.crear-dataframes.py
@@ -33,3 +33,33 @@ avocados_2019 = pd.DataFrame(avocados_list)
 
 # Print the new DataFrame
 print(avocados_2019)
+
+# ------------------------------------------------
+# Sección 2: Diccionario de listas
+# ------------------------------------------------
+# ¡Acaban de llegar más datos! Esta vez, utilizarás el método del diccionario de listas, analizando los datos
+# columna por columna.
+#
+# date          small_sold    large_sold
+# "2019-11-17"  10859987      7674135
+# "2019-12-01"  9291631       6238096
+#
+# Se ha importado pandas como pd.
+
+# Instrucciones:
+# - Crea un diccionario de listas con los nuevos datos llamado avocados_dict.
+# - Convierte el diccionario en un DataFrame llamado avocados_2019.
+# - Imprime tu nuevo DataFrame.
+
+# Create a dictionary of lists with new data
+avocados_dict = {
+    "date": ["2019-11-17","2019-12-01"],
+    "small_sold": [10859987,9291631],
+    "large_sold": [7674135,6238096]
+}
+
+# Convert dictionary into DataFrame
+avocados_2019 = pd.DataFrame(avocados_dict)
+
+# Print the new DataFrame
+print(avocados_2019)
